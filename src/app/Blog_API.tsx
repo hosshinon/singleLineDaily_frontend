326090fcbbfb23164154366b7190311b45a5feb8
hosshinon <155843587+hosshinon@users.noreply.github.com>
2024-03-05 10:13:54 +0900

    実験

--- src/app/Blog_API.tsx
+++ src/app/Blog_API.tsx
@@ -5,9 +5,12 @@ const API_ENDPOINT =
 
 //記事一覧取得
 export const getTodos = async ():Promise<Todo[]> => {
-  const res = await fetch(`${API_ENDPOINT}/api/v1/posts`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://easyblog-api-backend.onrender.com/api/v1/posts`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("エラーが発生しました");
@@ -21,9 +24,12 @@ export const getTodos = async ():Promise<Todo[]> => {
 
 //記事詳細取得
 export const getDetailTodo = async(id:string):Promise<Todo> => {
-  const res = await fetch(`${API_ENDPOINT}/api/v1/posts/${id}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://easyblog-api-backend.onrender.com/api/v1/posts/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("エラーが発生しました");
@@ -38,13 +44,16 @@ export const getDetailTodo = async(id:string):Promise<Todo> => {
 //記事投稿
 export const createTodo = async(title:string,content:string):Promise<Todo> => {
   
-  const res = await fetch(`${API_ENDPOINT}/api/v1/posts`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title, content }),
-  });
+  const res = await fetch(
+    `https://easyblog-api-backend.onrender.com//api/v1/posts`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content }),
+    }
+  );
 
   console.log(res)
 
@@ -61,12 +70,15 @@ export const createTodo = async(title:string,content:string):Promise<Todo> => {
 //記事削除
 export const deleteTodo = async(id:string):Promise<void> => {
   
-  const res = await fetch(`${API_ENDPOINT}/api/v1/posts/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const res = await fetch(
+    `https://easyblog-api-backend.onrender.com/api/v1/posts/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   console.log(res)
 
@@ -79,13 +91,16 @@ export const deleteTodo = async(id:string):Promise<void> => {
 //記事編集
 export const editTodo = async(id:string,title:string,content:string):Promise<Todo> => {
   
-  const res = await fetch(`${API_ENDPOINT}/api/v1/posts/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title, content }),
-  });
+  const res = await fetch(
+    `https://easyblog-api-backend.onrender.com/api/v1/posts/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content }),
+    }
+  );
 
   // console.log(id,title,content)
 
