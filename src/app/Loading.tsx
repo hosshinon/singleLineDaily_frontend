d7bdb191b0d34ec52dbd67236d5f85874f24e974
hosshinon <155843587+hosshinon@users.noreply.github.com>
2024-03-05 20:09:32 +0900

    ”ローティングアニメーション実装”

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Loading = () => {
 	return (
-		<div>Loading now</div>
-	)
+    <div className='flex justify-center' aria-label='読み込み中'>
+      <div className='animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent'></div>
+    </div>
+  );
 }
 
 export default Loading
