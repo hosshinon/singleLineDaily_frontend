72a422f11534c7e131e80d81c9fd7c73206bf139
hosshinon <155843587+hosshinon@users.noreply.github.com>
2024-03-05 22:33:40 +0900

    タイトルの位置を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   //console.log(todos);
   return (
     <main className='bg-gray-200 flex flex-col justify-center items-center min-h-screen py-2'>
-      <h1 className='text-4xl font-bold text-gray-600 -mt-32 '>
+      <h1 className='text-4xl font-bold text-gray-600  '>
         ひとこと日記 ( Next.js & RailsAPI)
       </h1>
       <div className='w-full max-w-xl mt-5 '>
