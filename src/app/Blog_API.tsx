0da32cbc15bea390d811966b56562800f4f6f1bc
hosshinon <hosshinon@gmail.com>
2024-11-17 06:37:02 +0900

    バックエンドAPIはローカルホスト3001

--- src/app/Blog_API.tsx
+++ src/app/Blog_API.tsx
@@ -1,91 +1,93 @@
-import { Todo } from "./type";
+import { Todo } from './type'
 
 const API_ENDPOINT =
-  process.env.NEXT_PUBLIC_API_ENDPOINT || "http://127.0.0.1:3001";
+  process.env.NEXT_PUBLIC_API_ENDPOINT || 'http://127.0.0.1:3001'
 
 //記事一覧取得
-export const getTodos = async ():Promise<Todo[]> => {
+export const getTodos = async (): Promise<Todo[]> => {
   const res = await fetch(`${API_ENDPOINT}/api/v1/posts`, {
-    cache: "no-store",
-  });
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
-    throw new Error("エラーが発生しました");
+    throw new Error('エラーが発生しました')
   }
 
   //await new Promise((resolve) => setTimeout(resolve, 1000));
-  const Todos = await res.json();
-  return Todos;
-};
+  const Todos = await res.json()
+  return Todos
+}
 
 //記事詳細取得
-export const getDetailTodo = async(id:string):Promise<Todo> => {
+export const getDetailTodo = async (id: string): Promise<Todo> => {
   const res = await fetch(`${API_ENDPOINT}/api/v1/posts/${id}`, {
-    cache: "no-store",
-  });
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
-    throw new Error("エラーが発生しました");
+    throw new Error('エラーが発生しました')
   }
 
   //await new Promise((resolve) => setTimeout(resolve, 1000));
-  const Todos = await res.json();
-  return Todos;
-};
+  const Todos = await res.json()
+  return Todos
+}
 
 //記事投稿
-export const createTodo = async(title:string,content:string):Promise<Todo> => {
-  
+export const createTodo = async (
+  title: string,
+  content: string
+): Promise<Todo> => {
   const res = await fetch(`${API_ENDPOINT}//api/v1/posts`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ title, content }),
-  });
+  })
 
   if (!res.ok) {
-    throw new Error("エラーが発生しました");
+    throw new Error('エラーが発生しました')
   }
 
   //await new Promise((resolve) => setTimeout(resolve, 1000));
-  const newTodo = await res.json();
-  return newTodo;
-};
+  const newTodo = await res.json()
+  return newTodo
+}
 
 //記事削除
-export const deleteTodo = async(id:string):Promise<void> => {
-  
+export const deleteTodo = async (id: string): Promise<void> => {
   const res = await fetch(`${API_ENDPOINT}/api/v1/posts/${id}`, {
-    method: "DELETE",
+    method: 'DELETE',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-  });
+  })
 
   if (!res.ok) {
-    throw new Error("エラーが発生しました");
+    throw new Error('エラーが発生しました')
   }
-
-};
+}
 
 //記事編集
-export const editTodo = async(id:string,title:string,content:string):Promise<Todo> => {
-  
+export const editTodo = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Todo> => {
   const res = await fetch(`${API_ENDPOINT}/api/v1/posts/${id}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ title, content }),
-  });
-
+  })
 
   if (!res.ok) {
-    throw new Error("エラーが発生しました");
+    throw new Error('エラーが発生しました')
   }
 
   //await new Promise((resolve) => setTimeout(resolve, 1000));
-  const editTodo = await res.json();
-  return editTodo;
-};
+  const editTodo = await res.json()
+  return editTodo
+}
